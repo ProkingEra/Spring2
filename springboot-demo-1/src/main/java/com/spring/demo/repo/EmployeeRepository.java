a251e72b46f0668aa77cb14071fa1eadb27fb051
ProkingEra <51392589+ProkingEra@users.noreply.github.com>
2023-03-08 13:01:07 -0500

    Add files via upload

---
+++ springboot-demo-1/src/main/java/com/spring/demo/repo/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.spring.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
+
+import com.spring.demo.employee.Employee;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
---
+++ springboot-demo-1/src/main/java/com/spring/demo/service/EmployeeService.java
@@ -0,0 +1,14 @@
+//package com.spring.demo.service;
+//
+//import java.util.*;
+//
+//import com.spring.demo.employee.Employee;
+//
+//public interface EmployeeService {
+//	
+//	Employee saveEmployee(Employee employee);
+//	List<Employee> getAllEmployees();
+//	Employee getEmployeeById(long id);
+//	Employee updateEmployee(Employee employee, long id);
+//	void deleteEmployee(long id);
+//}
